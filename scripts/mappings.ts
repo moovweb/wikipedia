6d97deb2dfbb39a64ffa16472b671e184fcb52a0
Michael JL Catlin <malrase@gmail.com>
2012-06-26 12:40:58 -0700

    Upgrading the project to the new style

--- scripts/mappings.ts
+++ scripts/mappings.ts
@@ -1,10 +1,11 @@
 match($path) {
-	# When the path has "Main_Page", import our home Tritium
-	with(/Main_Page/) {
-		log("--> Importing home.ts")
-		@import "home.ts"
-	}
-	else() {
-		log("Not importing")
-	}
+  # When the path has "Main_Page", import our home Tritium
+  with(/Main_Page/) {
+    log("--> Importing home.ts")
+    @import "home.ts"
+  }
+  else() {
+    log("Not importing")
+  }
 }
+

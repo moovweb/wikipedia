51bd1bf375b4ef94e104bcec8129b2362a261eab
Michael JL Catlin <malrase@gmail.com>
2012-07-26 14:19:47 -0700

    Using CSS selectors

--- scripts/home.ts
+++ scripts/home.ts
@@ -1,16 +1,16 @@
-$("/html/body") {
+$$("body") {
   # Give the body an id, allowing page-specific styling
   attribute("id", "_homepage")
   # Removing the ugly top banner
-  $(".//table[@id='mp-topbanner']") {
+  $$("#mp-topbanner") {
     remove()
   }
   # Moving the featured image to the top of the page
-  $(".//table[@id='mp-lower']") {
+  $$("#mp-lower") {
     move_to("..", "top")
   }
   # Adding our own class, as well as removing inline styles
-  $(".//div[@class='mw-body']//table//h2") {
+  $$(".mw-body h2") {
     attribute("class", "pill")
     remove("@style")
   }
